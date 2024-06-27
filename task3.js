21b9a7135865e264b1df5819a6631a4cd2a473dd
Megha1904 <megha200328@gmail.com>
2024-06-27 16:46:55 +0530

    message

---
+++ task3.js
@@ -0,0 +1,22 @@
+var str = "Hello"
+const score = 100
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const heros = ["IronMan", "CaptainAmerica", "Thor"];
+var obj = { key: "value", age: 30 };
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+console.log("str:", str, typeof str);
+console.log("score:", score, typeof score);
+console.log("num:", num, typeof num);
+console.log("isLoggedIn:", isLoggedIn, typeof isLoggedInd);
+console.log("userEmail:", userEmail, typeof userEmail);
+console.log("heros:", heros, typeof heros);
+console.log("obj:", obj, typeof obj);
+console.log("greet:", greet, typeof greet);
+
+
